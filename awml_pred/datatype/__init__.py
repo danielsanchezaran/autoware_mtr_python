381f29a35bb3866756450dbb1575017f4b0004a9
Daniel <danielsanchezaran@gmail.com>
2025-03-11 15:58:05 +0900

    fix polyline types
    Signed-off-by: Daniel <danielsanchezaran@gmail.com>

--- awml_pred/datatype/__init__.py
+++ awml_pred/datatype/__init__.py
@@ -1,53 +1,46 @@
-from .agents import AgentType, ArgoverseAgent, T4Agent, WaymoAgent
-from .base import ContextType, LabelType
-from .dataset import DatasetName
-from .polylines import (
-    ArgoverseLane,
-    ArgoversePolyline,
-    ArgoverseRoadEdge,
-    ArgoverseRoadLine,
-    BoundaryType,
-    LaneType,
-    PolylineType,
-    SignalType,
-    T4Lane,
-    T4Polyline,
-    T4RoadEdge,
-    T4RoadLine,
-    WaymoLane,
-    WaymoPolyline,
-    WaymoRoadEdge,
-    WaymoRoadLine,
-    WaymoSignal,
-)
+from .agents import AgentType
+from .context import *
+from .dataset import *
+from .context import *  # noqa
+from .map import *  # noqa
+# from .polylines import (
+#     BoundaryType,
+#     LaneType,
+#     PolylineType,
+#     SignalType,
+#     T4Lane,
+#     T4Polyline,
+#     T4RoadEdge,
+#     T4RoadLine,
+# )
 
-__all__ = (
-    "AgentType",
-    "ArgoverseAgent",
-    "ArgoverseLane",
-    "ArgoversePolyline",
-    "ArgoverseRoadEdge",
-    "ArgoverseRoadLine",
-    "BoundaryType",
-    "ContextType",
-    "LabelType",
-    "LaneType",
-    "PolylineType",
-    "SignalType",
-    "T4Agent",
-    "T4Lane",
-    "T4Polyline",
-    "T4RoadEdge",
-    "T4RoadLine",
-    "WaymoLane",
-    "WaymoAgent",
-    "WaymoPolyline",
-    "WaymoRoadEdge",
-    "WaymoRoadLine",
-    "WaymoSignal",
-    "get_focal_agent_types",
-    "DatasetName",
-)
+# __all__ = (
+#     "AgentType",
+#     "ArgoverseAgent",
+#     "ArgoverseLane",
+#     "ArgoversePolyline",
+#     "ArgoverseRoadEdge",
+#     "ArgoverseRoadLine",
+#     "BoundaryType",
+#     "ContextType",
+#     "LabelType",
+#     "LaneType",
+#     "PolylineType",
+#     "SignalType",
+#     "T4Agent",
+#     "T4Lane",
+#     "T4Polyline",
+#     "T4RoadEdge",
+#     "T4RoadLine",
+#     "WaymoLane",
+#     "WaymoAgent",
+#     "WaymoPolyline",
+#     "WaymoRoadEdge",
+#     "WaymoRoadLine",
+#     "WaymoSignal",
+#     "get_focal_agent_types",
+#     "DatasetName",
+# )
 
 
 def get_focal_agent_types() -> list[str]:
--- utils/lanelet_converter.py
+++ utils/lanelet_converter.py
@@ -18,8 +18,9 @@ except ImportError as e:
 
 from awml_pred.common import uuid
 from awml_pred.dataclass import AWMLStaticMap, BoundarySegment, CrosswalkSegment, LaneSegment, Polyline
-from awml_pred.datatype import BoundaryType, T4Lane, T4Polyline, T4RoadEdge, T4RoadLine
-from awml_pred.typing import NDArray
+from awml_pred.datatype import MapType
+from .constant import MAP_TYPE_MAPPING, T4_LANE, T4_ROADEDGE, T4_ROADLINE
+
 # cspell: ignore MGRS
 
 
@@ -119,14 +120,14 @@ def _is_roadedge_linestring(line_type: str, _line_subtype: str) -> bool:
 
     Returns:
     -------
-        bool: Return True if line type is contained in T4RoadEdge.
+        bool: Return True if line type is contained in T4_ROADEDGE.
 
     Note:
     ----
         Currently `_line_subtype` is not used, but it might be used in the future.
 
     """
-    return line_type.upper() in T4RoadEdge.__members__
+    return line_type in T4_ROADEDGE
 
 
 def _is_roadline_linestring(_line_type: str, line_subtype: str) -> bool:
@@ -139,14 +140,14 @@ def _is_roadline_linestring(_line_type: str, line_subtype: str) -> bool:
 
     Returns:
     -------
-        bool: Return True if line subtype is contained in T4RoadLine.
+        bool: Return True if line subtype is contained in T4_RoadLine.
 
     Note:
     ----
         Currently `_line_type` is not used, but it might be used in the future.
 
     """
-    return line_subtype.upper() in T4RoadLine.__members__
+    return line_subtype in T4_ROADLINE
 
 
 def _get_boundary_type(linestring: lanelet2.core.LineString3d) -> BoundaryType:
@@ -164,16 +165,16 @@ def _get_boundary_type(linestring: lanelet2.core.LineString3d) -> BoundaryType:
     line_type = _get_linestring_type(linestring)
     line_subtype = _get_linestring_subtype(linestring)
     if _is_virtual_linestring(line_type, line_subtype):
-        return T4RoadLine.VIRTUAL
+        return MapType.UNKNOWN
     elif _is_roadedge_linestring(line_type, line_subtype):
-        return T4RoadEdge.from_str(line_type)
+        return MAP_TYPE_MAPPING[line_type]
     elif _is_roadline_linestring(line_type, line_subtype):
-        return T4RoadLine.from_str(line_subtype)
+        return MAP_TYPE_MAPPING[line_subtype]
     else:
         logging.warning(
-            f"[Boundary]: id={linestring.id}, type={line_type}, subtype={line_subtype}, T4RoadLine.VIRTUAL is used.",
+            f"[Boundary]: id={linestring.id}, type={line_type}, subtype={line_subtype}, MapType.UNKNOWN is used.",
         )
-        return T4RoadLine.VIRTUAL
+        return MapType.UNKNOWN
 
 
 def _get_boundary_segment(linestring: lanelet2.core.LineString3d) -> BoundarySegment:
@@ -190,9 +191,8 @@ def _get_boundary_segment(linestring: lanelet2.core.LineString3d) -> BoundarySeg
     """
     boundary_type = _get_boundary_type(linestring)
     waypoints = _interpolate_lane(np.array([(line.x, line.y, line.z) for line in linestring]))
-    global_type = T4Polyline.from_str(boundary_type.as_str())
-    polyline = Polyline(polyline_type=global_type, waypoints=waypoints)
-    return BoundarySegment(linestring.id, boundary_type, polyline)
+    polyline = Polyline(polyline_type=boundary_type, waypoints=waypoints)
+    return BoundarySegment(linestring.id, polyline)
 
 
 def _get_speed_limit_mph(lanelet: lanelet2.core.Lanelet) -> float | None:
@@ -327,17 +327,14 @@ def convert_lanelet(filename: str) -> AWMLStaticMap:
     taken_boundary_ids: list[int] = []
     for lanelet in lanelet_map.laneletLayer:
         lanelet_subtype = _get_lanelet_subtype(lanelet)
-        if lanelet_subtype == "":
-            continue
 
         # NOTE: skip walkway because it contains stop_line as boundary
-        if T4Lane.contains(lanelet_subtype) and lanelet_subtype != "walkway":
+        if lanelet_subtype in T4_LANE:
             # lane
-            lane_type = T4Lane.from_str(lanelet_subtype)
+            lane_type = MAP_TYPE_MAPPING[lanelet_subtype]
             lane_waypoints = _interpolate_lane(
                 np.array([(line.x, line.y, line.z) for line in lanelet.centerline]))
-            global_lane_type = T4Polyline.from_str(lanelet_subtype)
-            lane_polyline = Polyline(polyline_type=global_lane_type, waypoints=lane_waypoints)
+            lane_polyline = Polyline(polyline_type=lane_type, waypoints=lane_waypoints)
             is_intersection = _is_intersection(lanelet)
             left_neighbor_ids, right_neighbor_ids = _get_left_and_right_neighbor_ids(
                 lanelet, routing_graph)
@@ -351,7 +348,6 @@ def convert_lanelet(filename: str) -> AWMLStaticMap:
 
             lane_segments[lanelet.id] = LaneSegment(
                 id=lanelet.id,
-                lane_type=lane_type,
                 polyline=lane_polyline,
                 is_intersection=is_intersection,
                 left_boundaries=[left_boundary],
@@ -363,7 +359,7 @@ def convert_lanelet(filename: str) -> AWMLStaticMap:
         elif lanelet_subtype == "crosswalk":
             waypoints = _interpolate_lane(
                 np.array([(poly.x, poly.y, poly.z) for poly in lanelet.polygon3d()]))
-            polygon = Polyline(polyline_type=T4Polyline.CROSSWALK, waypoints=waypoints)
+            polygon = Polyline(polyline_type=MAP_TYPE_MAPPING[lanelet_subtype], waypoints=waypoints)
             crosswalk_segments[lanelet.id] = CrosswalkSegment(lanelet.id, polygon)
         else:
             logging.warning(f"[Lanelet]: {lanelet_subtype} is unsupported and skipped.")
@@ -372,9 +368,7 @@ def convert_lanelet(filename: str) -> AWMLStaticMap:
     boundary_segments: dict[int, BoundarySegment] = {}
     for linestring in lanelet_map.lineStringLayer:
         type_name: str = _get_linestring_type(linestring)
-        if (
-            T4RoadEdge.contains(type_name) or T4RoadLine.contains(type_name)
-        ) and linestring.id not in taken_boundary_ids:
+        if (type_name in T4_ROADEDGE or type_name in T4_ROADLINE) and linestring.id not in taken_boundary_ids:
             boundary_segments[linestring.id] = _get_boundary_segment(linestring)
 
     # generate uuid from map filepath
